063389256568a61fd3197cd1f86e7cbb0e308c43
autonote10026 <53842671+autonote10026@users.noreply.github.com>
2020-08-11 15:45:50 +1000

    WK2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
+        //window.run();
     }
+    public class Canvas extends JPanel{
+        Grid grid;
+        
+        public Canvas(){
+            setPreferredSize(new Dimension(720,720));
+            grid = new Grid(); 
+        }
+  
+@Override
+public void paint (Graphics g){
+grid.paint(g, getMousePosition());
 }
+    }
+
+ 
+    
+
+    public Main(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+    }
+
+   
+}
+
+
+
+
+
+
