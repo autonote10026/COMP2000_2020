19385fc2e54761d3fd6109c38f69c68b696bba6e
autonote10026 <53842671+autonote10026@users.noreply.github.com>
2020-08-18 14:02:39 +1000

    updated

--- src/Grid.java
+++ src/Grid.java
@@ -1,9 +1,28 @@
-class Grid {
-}
-}
-}
+import java.awt.*;
 
-public Cell cellAtColRow(int c, int r){
-return cells[c][r];
-}
+class Grid {
+    //fields
+    Cell[][] cells = new Cell[20][20];
+
+    // constructor
+    public Grid(){
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                cells[i][j] = new Cell(10+35*i,10+35*j);
+            }
+        }
+    }
+
+    // methods
+    public void paint(Graphics g, Point mousePos){
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                cells[i][j].paint(g, mousePos);
+            }
+        }
+    }
+
+    public Cell cellAtColRow(int c, int r){
+        return cells[c][r];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-class App extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+class Main extends JFrame {
+    
+    class App extends JPanel {
         
-    Stage stage;
+        Stage stage;
 
-    public App() {
-        setPreferredSize(new Dimension(720, 720));
-        stage = new Stage();
+        public App() {
+            setPreferredSize(new Dimension(720, 720));
+            stage = new Stage();
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            stage.paint(g, getMousePosition());
+        }
+
     }
 
-    @Override
-    public void paint(Graphics g) {
-        stage.paint(g, getMousePosition());
+    public static void main(String[] args) throws Exception {
+        Main window = new Main();
+        window.run();
     }
 
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        App canvas = new App();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public void run() {
+        while (true) {
+            this.repaint();
+        }
+    }
 }
